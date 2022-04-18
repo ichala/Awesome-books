2a389a9442b066dee904a394af24fd7977e75743
ichala <alijandhd@gmail.com>
2022-04-18 11:08:57 +0100

    Add Display Book Function

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
-const ourBook = [{
-  id: 1,
-  name: "Book1",
-  author: "Author1"
-}, {
-  id: 2,
-  name: "Book2",
-  author: "Author2"
-}]
+const ourBooks = [
+  {
+    id: 1,
+    name: "Book1",
+    author: "Author1",
+  },
+  {
+    id: 2,
+    name: "Book2",
+    author: "Author2",
+  },
+  {
+    id: 3,
+    name: "Book3",
+    author: "Author3",
+  },
+  {
+    id: 4,
+    name: "Book4",
+    author: "Author4",
+  },
+  {
+    id: 5,
+    name: "Book5",
+    author: "Autho5",
+  },
+];
+
+// Display Books
+const BookList = document.querySelector("#book-list");
+ourBooks.forEach((book) => {
+  let new_book = ` 
+ <p>${book.name}</p>
+ <p>${book.author}</p>
+ <button onclick=RemoveBook(${book.id}) >Remove</button>
+ <hr>
+ `;
+  let Singlebook = document.createElement("div");
+  Singlebook.classList.add("single-book");
+  Singlebook.innerHTML = new_book;
+  BookList.appendChild(Singlebook);
+});
+
+// Remove Books
+
+// Add Books
