ea05b728b3335067a66573b131f8f1a75eea19ab
ichala <alijandhd@gmail.com>
2022-04-20 08:40:51 +0100

    Add Last Modified And navigation Menu

--- refactor.js
+++ refactor.js
@@ -72,33 +72,33 @@ function RemoveBook(id) {
   books.delete(id);
 }
 
-// // Last modified
-// const lastMod = document.querySelector('#last-modified');
-// lastMod.innerHTML = document.lastModified;
+// Last modified
+const lastMod = document.querySelector('#last-modified');
+lastMod.innerHTML = document.lastModified;
 
-// // Navigation
-// const navListBtn = document.querySelector('#nav-list');
-// const navAddNewBtn = document.querySelector('#nav-add-new');
-// const navContactBtn = document.querySelector('#nav-contact');
+// Navigation
+const navListBtn = document.querySelector('#nav-list');
+const navAddNewBtn = document.querySelector('#nav-add-new');
+const navContactBtn = document.querySelector('#nav-contact');
 
-// const listSection = document.querySelector('#list');
-// const addNewSection = document.querySelector('#add-new');
-// const contactSection = document.querySelector('#contact');
+const listSection = document.querySelector('#list');
+const addNewSection = document.querySelector('#add-new');
+const contactSection = document.querySelector('#contact');
 
-// navListBtn.addEventListener('click', () => {
-//   listSection.style.display = 'block';
-//   addNewSection.style.display = 'none';
-//   contactSection.style.display = 'none';
-// });
+navListBtn.addEventListener('click', () => {
+  listSection.style.display = 'block';
+  addNewSection.style.display = 'none';
+  contactSection.style.display = 'none';
+});
 
-// navAddNewBtn.addEventListener('click', () => {
-//   addNewSection.style.display = 'block';
-//   listSection.style.display = 'none';
-//   contactSection.style.display = 'none';
-// });
+navAddNewBtn.addEventListener('click', () => {
+  addNewSection.style.display = 'block';
+  listSection.style.display = 'none';
+  contactSection.style.display = 'none';
+});
 
-// navContactBtn.addEventListener('click', () => {
-//   contactSection.style.display = 'block';
-//   addNewSection.style.display = 'none';
-//   listSection.style.display = 'none';
-// });
+navContactBtn.addEventListener('click', () => {
+  contactSection.style.display = 'block';
+  addNewSection.style.display = 'none';
+  listSection.style.display = 'none';
+});
